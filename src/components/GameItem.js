761ea3f614a4cdec0ab802d04234a5464f7c8cee
Amy Phung <phung.amy85@gmail.com>
2019-06-12 16:39:29 -0700

    select correct icon for each item

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -16,8 +16,9 @@ class GameItem extends Component {
     };
 
     // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
 
+    let icon = ItemIcons[this.props.type];
+    
     return (
       <div className="game-item" style={itemStyle}>
         <img src={icon} alt="Item" className="icon-item"></img>

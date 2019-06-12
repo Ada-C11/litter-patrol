49f2a4180eed1a0e38a824e5bb60cef7b62493c9
Shubha Rajan <shubharajan94@gmail.com>
2019-06-12 16:17:07 -0700

    added check marks and x display for when items are spotted

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,22 +4,47 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSpotted:false,
+    };
+  }
+  
   propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
   }
 
+  onItemClick = () => {
+    this.setState({
+      isSpotted: true,
+    });
+  }
+
   render() {
+
+    let spottedType = undefined;
+    if (this.state.isSpotted) {
+      if (this.props.type === 'litter') {
+        spottedType = 'spotted-litter';
+      } else {
+        spottedType ='spotted-nature';
+      }
+    }
+    
+
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
+      
     };
 
     // Update this to select the correct icon for each item
     const icon = ItemIcons[this.props.type];
 
     return (
-      <div className="game-item" style={itemStyle}>
+      <div className={`game-item ${spottedType}`} style={itemStyle} onClick={this.onItemClick}>
         <img src={icon} alt="Item" className="icon-item"></img>
       </div>
     );

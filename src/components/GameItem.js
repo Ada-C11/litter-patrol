008deb2ce436f22c021bda34212d8a85d56e391c
Pauline Sauget <paulinevsauget@gmail.com>
2019-06-12 14:39:07 -0700

    Display correct icon for each item: GameItem inherits props; `type` is passed in as state data

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,6 +4,10 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
   propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
@@ -16,7 +20,8 @@ class GameItem extends Component {
     };
 
     // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    // const icon = ItemIcons.rock;
+    const icon = ItemIcons[this.props.type]
 
     return (
       <div className="game-item" style={itemStyle}>

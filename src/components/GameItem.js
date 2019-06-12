f5fd47bd97e2aacf038d4395fdbb6cf0008985f5
Kate Sandler <kate@katekatekate.net>
2019-06-12 15:34:01 -0700

    gets item type from props

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -6,21 +6,28 @@ import PropTypes from 'prop-types';
 class GameItem extends Component {
   propTypes = {
     height: PropTypes.number.isRequired,
-    layer: PropTypes.number.isRequired,
-  }
+    layer: PropTypes.number.isRequired
+  };
 
   render() {
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
-      zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
+      zIndex: this.props.layer // use props.layer to set z-index, so we display ontop of background
     };
 
-    // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    // TODO: Update this to select the correct icon for each item
+    const type = this.props.type;
+    const icon = ItemIcons[type];
+    const itemClass = 'game-item';
 
     return (
-      <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
+      <div className={itemClass} style={itemStyle}>
+        <img
+          src={icon}
+          alt={type}
+          className="icon-item"
+          onClick={this.onClick}
+        />
       </div>
     );
   }

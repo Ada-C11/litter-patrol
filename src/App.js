f302fd5b8b5695ae04d48b8762ae04fb2c1efbcf
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-06-12 13:57:41 -0700

    added functionality to display the proper icon instead of just rocks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import uuid from 'uuid';
 import './App.css';
 import GameItem from './components/GameItem.js';
 import logo from './images/logo.png';
+// import ItemIcons from './ItemIcons.js';
 
 class App extends Component {
   config = {
@@ -30,8 +31,8 @@ class App extends Component {
     };
 
     // Uncomment this to spawn a single test item
-    //const testItem = this.spawnItem(Date.now());
-    //this.state.items.push(testItem);
+    // const testItem = this.spawnItem(Date.now());
+    // this.state.items.push(testItem);
 
     // Uncomment this to automatically spawn new items
     this.enableSpawner();
@@ -49,6 +50,7 @@ class App extends Component {
                height={item.height}     // Height - used for a CSS style to position on the screen
                layer={100 + i}          // Layer - used for a CSS style to show items on-top of bg
                key={item.id}            // Key - to help React with performance
+               type={item.type}
 
                // Additional props (event callbacks, etc.) can be passed here
              />;
--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,9 +4,10 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
-  propTypes = {
+  static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
   }
 
   render() {
@@ -15,8 +16,9 @@ class GameItem extends Component {
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
 
-    // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    // Updated the icon type
+    const icon = ItemIcons[this.props.type];
+   
 
     return (
       <div className="game-item" style={itemStyle}>

a22a38186455d1ec408ad45547a98175958298db
rosalyn <rosalyn.anuseh@gmail.com>
2019-06-12 20:15:00 -0700

    first commit, forgot to commit

--- src/App.js
+++ src/App.js
@@ -1,75 +1,75 @@
-import React, { Component } from 'react';
-import uuid from 'uuid';
-import './App.css';
-import GameItem from './components/GameItem.js';
-import logo from './images/logo.png';
+import React, { Component } from "react";
+import uuid from "uuid";
+import "./App.css";
+import GameItem from "./components/GameItem.js";
+import logo from "./images/logo.png";
 
 class App extends Component {
   config = {
     itemTypes: {
       // type: spawn rate (weighting)
-      litter:  20,
-      rock:     5,
-      bush:     5,
-      flower:   5,
-      mushroom: 5,
+      litter: 20,
+      rock: 5,
+      bush: 5,
+      flower: 5,
+      mushroom: 5
     },
     spawnRate: 1.2, // Hz
     spawnRateRnd: 1.79, // randomization factor
     spawnHeight: 100, // height of item spawn area in pixels
     spawnFloor: 0, // offset from bottom of game "level" in pixels
-    itemLifetime: 10 * 1000, // 10 seconds (should be longer than CSS animation time)
-  }
+    itemLifetime: 10 * 1000 // 10 seconds (should be longer than CSS animation time)
+  };
 
   constructor() {
     super();
 
     this.state = {
       items: [],
-      points: 0,
+      points: 0
     };
 
     // Uncomment this to spawn a single test item
-    //const testItem = this.spawnItem(Date.now());
-    //this.state.items.push(testItem);
+    const testItem = this.spawnItem(Date.now());
+    this.state.items.push(testItem);
 
     // Uncomment this to automatically spawn new items
     this.enableSpawner();
 
-    console.log(this.state);
+    this.onItemClicked = this.onItemClicked.bind(this);
   }
 
   onItemClicked = () => {
-    // Fill this in!
-  }
+    this.setState({ points: 1});
+  };
 
   render() {
-    const items = this.state.items.map((item, i) => {
-      return <GameItem
-               height={item.height}     // Height - used for a CSS style to position on the screen
-               layer={100 + i}          // Layer - used for a CSS style to show items on-top of bg
-               key={item.id}            // Key - to help React with performance
 
-               // Additional props (event callbacks, etc.) can be passed here
-             />;
+    const items = this.state.items.map((item, i) => {
+      return (
+        <GameItem
+          height={item.height} // Height - used for a CSS style to position on the screen
+          layer={100 + i} // Layer - used for a CSS style to show items on-top of bg
+          key={item.id} // Key - to help React with performance
+          item={item} // Additional props (event callbacks, etc.) can be passed here
+        />
+      );
     });
 
     return (
       <div className="game">
         <section className="hud">
-          <h2 className="score">Litter Spotted: { this.state.points }</h2>
+          <h2 className="score">Litter Spotted: {this.state.points}</h2>
           <img className="logo" src={logo} alt="Litter Patrol logo" />
         </section>
 
         <section className="level">
-          { this.levelBackground() }
-          { items }
+          {this.levelBackground()}
+          <div onClick={this.onItemClicked}>{items}</div>
         </section>
-
       </div>
     );
   }
-
 
   //////////////\\\\\\\\\\\\\\
   // Implementation details \\
@@ -78,29 +78,29 @@ class App extends Component {
     const newState = {};
 
     // Cull any items that are expired
-    const items = this.state.items.filter((item) => {
+    const items = this.state.items.filter(item => {
       return item.expiration !== null && item.expiration > time;
     });
 
-    if(items.length !== this.state.items.length) {
+    if (items.length !== this.state.items.length) {
       newState.items = items;
     }
 
     // Should we spawn a new item?
-    const {spawnRate, spawnRateRnd} = this.config;
-    if(this.spawnItems && spawnRate > 0) {
+    const { spawnRate, spawnRateRnd } = this.config;
+    if (this.spawnItems && spawnRate > 0) {
       let spawnDelta = time - (this.lastSpawn || 0);
 
       // Randomize spawn rate
-      if(spawnRateRnd > 0) {
+      if (spawnRateRnd > 0) {
         const factor = 1 + Math.random() * spawnRateRnd;
         spawnDelta *= factor;
       }
 
-      if(spawnDelta >= (1 / spawnRate) * 1000) {
+      if (spawnDelta >= (1 / spawnRate) * 1000) {
         newState.items = [
           ...(newState.items || this.state.items),
-          this.spawnItem(time),
+          this.spawnItem(time)
         ];
       }
     }
@@ -116,14 +116,18 @@ class App extends Component {
     const type = this.randomType();
 
     const expiration = time + this.config.itemLifetime;
-    const height = Math.random() * this.config.spawnHeight + this.config.spawnFloor;
+    const height =
+      Math.random() * this.config.spawnHeight + this.config.spawnFloor;
 
-    return {id, type, expiration, height};
+    return { id, type, expiration, height };
   }
 
   randomType() {
     // Figure out the total of all the weighted types
-    const totalWeight = Object.values(this.config.itemTypes).reduce((l,r)=>l+r,0);
+    const totalWeight = Object.values(this.config.itemTypes).reduce(
+      (l, r) => l + r,
+      0
+    );
 
     // Get a random value between zero and the total
     let choice = Math.random() * totalWeight;
@@ -131,10 +135,10 @@ class App extends Component {
 
     // Loop through all item types and figure out which one we chose
     Object.entries(this.config.itemTypes).forEach(([type, weight]) => {
-      if(selectedType !== null) return; // We've already found our choice
+      if (selectedType !== null) return; // We've already found our choice
 
       // If the random value was less than this type's weight
-      if(choice <= weight) {
+      if (choice <= weight) {
         selectedType = type; // then we've selected it
       } else {
         choice -= weight; // otherwise move past this entry
@@ -149,11 +153,23 @@ class App extends Component {
   }
 
   levelBackground() {
-    const layers = ['clouds-1', 'clouds-2', 'clouds-3', 'clouds-4',
-                    'hills-1','hills-2','bushes','trees-1','trees-2','ground'];
+    const layers = [
+      "clouds-1",
+      "clouds-2",
+      "clouds-3",
+      "clouds-4",
+      "hills-1",
+      "hills-2",
+      "bushes",
+      "trees-1",
+      "trees-2",
+      "ground"
+    ];
     return (
       <div className="level-bg">
-        {layers.map(layer => (<div className={`level-bg-${layer}`} key={layer} />))}
+        {layers.map(layer => (
+          <div className={`level-bg-${layer}`} key={layer} />
+        ))}
       </div>
     );
   }
@@ -167,7 +183,7 @@ class App extends Component {
 
       const newState = this.tick(Date.now());
 
-      if(Object.keys(newState).length > 0) {
+      if (Object.keys(newState).length > 0) {
         this.setState(newState, callback);
       } else {
         callback();
@@ -178,7 +194,7 @@ class App extends Component {
   }
 
   componentWillUnmount() {
-    if(this.updateTimer !== null) {
+    if (this.updateTimer !== null) {
       clearInterval(this.updateTimer);
     }
   }
--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
-import '../App.css';
-import ItemIcons from '../ItemIcons.js';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import "../App.css";
+import ItemIcons from "../ItemIcons.js";
+import PropTypes from "prop-types";
 
 class GameItem extends Component {
-  propTypes = {
-    height: PropTypes.number.isRequired,
-    layer: PropTypes.number.isRequired,
-  }
 
   render() {
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
-      zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
+      zIndex: this.props.layer // use props.layer to set z-index, so we display ontop of background
     };
 
     // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    const icon = ItemIcons[this.props.item.type];
+    const spotted = this.props.item.type === "litter" ? "spotted-litter" : "spotted-nature"
+
 
     return (
-      <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
+      <div className={`${spotted} game-item`} style={itemStyle}>
+        <img src={icon} alt="Item" className="icon-item" />
       </div>
     );
   }
+
+  static propTypes = {
+    height: PropTypes.number.isRequired,
+    layer: PropTypes.number.isRequired
+  };
 }
 
 export default GameItem;

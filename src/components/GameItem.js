442b2b0d53ef3e3d7b85ba8d3717b856714d2898
Kelly Downes <kedownes@gmail.com>
2019-06-13 09:05:24 -0700

    Add logic for checking spotted items

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,9 +4,23 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      spotted: false,
+    };
+  }
+
   static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
+  }
+
+  onItemClick = () => {
+    this.setState({
+      spotted: true
+    })
   }
 
   render() {
@@ -15,12 +29,21 @@ class GameItem extends Component {
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
 
-    // Update this to select the correct icon for each item
     const icon = ItemIcons[this.props.itemIcon];
 
+    let iconClass = 'game-item';
+
+    if (this.state.spotted) {
+      if (this.props.itemIcon === 'litter') {
+        iconClass += ` spotted-litter`;
+      } else {
+        iconClass += ` spotted-nature`;
+      }
+    }
+
     return (
-      <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
+      <div className={iconClass} style={itemStyle}>
+        <img onClick={ this.onItemClick } src={icon} alt="Item" className="icon-item"></img>
       </div>
     );
   }

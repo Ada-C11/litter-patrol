9c6bf76a86ac6cf34244518fa25d85fb41aca0bb
Chantal Demissie <chantaldemissie@yahoo.com>
2019-06-12 23:25:32 -0700

    players can spot items

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -10,22 +10,47 @@ class GameItem extends Component {
     layer: PropTypes.number.isRequired,
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {clicked: false};
+
+    // This binding is necessary to make `this` work in the callback.
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      clicked: true
+    }));
+  }
+
   render() {
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
-      
     };
 
     //wave 1
     // Update this to select the correct icon for each item
     const icon = ItemIcons[this.props.type];
+    const typeClass = this.props.type === "litter" ? "spotted-litter" : "spotted-nature";
 
-    return (
-      <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
-      </div>
-    );
+    if (this.state.clicked) {
+      return (
+        <div className="game-item" style={itemStyle}>
+          <div className={typeClass}></div>
+          <img src={icon} alt="Item" className="icon-item"></img>
+        </div>
+      );
+    } else {
+      return (
+        <div className="game-item" style={itemStyle} onClick={this.handleClick}>
+          <img src={icon} alt="Item" className="icon-item"></img>
+        </div>
+      );
+    }
+    
   }
 }
 

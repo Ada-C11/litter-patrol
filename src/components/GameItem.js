7747f20eb2ce5b35e0415a735d0c30e95d0d5426
sjscotton <sj.scotton@gmail.com>
2019-06-12 19:53:23 -0700

    cleanup

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -8,7 +8,6 @@ class GameItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: this.props.item,
       clicked: false,
     };
   }
@@ -19,7 +18,7 @@ class GameItem extends Component {
 
   onGameItemClick = () => {
     console.log('Item clicked!');
-    if (!this.state.clicked && this.state.item === 'litter') this.props.incrementScoreFunc();
+    if (!this.state.clicked && this.props.item === 'litter') this.props.incrementScoreFunc();
     this.setState({
       clicked: true,
     })
@@ -32,17 +31,11 @@ class GameItem extends Component {
     };
 
     // Update this to select the correct icon for each item
-    // const icon = ItemIcons.rock;
-    const icon = ItemIcons[this.state.item];
+    const icon = ItemIcons[this.props.item];
 
     let status = '';
     if (this.state.clicked) {
-      status = (this.state.item === 'litter') ? 'spotted-litter' : 'spotted-nature';
-      // if (this.state.item === 'litter') {
-      //   status = 'spotted-litter'
-      // } else {
-      //   status = 'spotted-nature'
-      // }
+      status = (this.props.item === 'litter') ? 'spotted-litter' : 'spotted-nature';
     }
     return (
       <div className={`game-item ${status}`} style={itemStyle} onClick={this.onGameItemClick}>

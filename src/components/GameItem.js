d120d9cfc0dc4825aff49bc37b320d7272a7a581
Amy Wyatt <amy.wyatt12@outlook.com>
2019-06-13 21:43:09 -0700

    can track clicks

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,10 +4,14 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
-  propTypes = {
+  static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
     type: PropTypes.string.isRequired,
+  }
+
+  onItemClicked = () => {
+    console.log("This item was clicked");
   }
 
   render() {
@@ -16,11 +20,10 @@ class GameItem extends Component {
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
 
-    // Update this to select the correct icon for each item
     const icon = ItemIcons[this.props.type];
 
     return (
-      <div className="game-item" style={itemStyle}>
+      <div className="game-item" style={itemStyle} onClick={this.onItemClicked}>
         <img src={icon} alt="Item" className="icon-item"></img>
       </div>
     );

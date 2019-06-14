cd977ddc823d83be92f951fad5638387943e4214
Amy Wyatt <amy.wyatt12@outlook.com>
2019-06-13 21:31:38 -0700

    showing correct game icons

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -7,6 +7,7 @@ class GameItem extends Component {
   propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
   }
 
   render() {
@@ -16,7 +17,7 @@ class GameItem extends Component {
     };
 
     // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    const icon = ItemIcons[this.props.type];
 
     return (
       <div className="game-item" style={itemStyle}>

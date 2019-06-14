61f072645bbee8485225386049b1d9c6942590ad
Jansen <v.jansen.martin@gmail.com>
2019-06-14 09:50:54 -0700

    Remove unused state

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -24,7 +24,6 @@ class GameItem extends Component {
     this.props.onItemClickedCallback(this.props.icon);
 
     this.setState({
-      isSpotted: true,
       spottedType: spottedType,
     });
 
